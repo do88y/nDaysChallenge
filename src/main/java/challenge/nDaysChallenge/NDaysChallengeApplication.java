7abd35131ecfccdc093c49f3e90c8f6f3d3d8962
do88y <appassionato.seo@gmail.com>
2022-10-26 18:51:45 +0900

    스프링 부트 프로젝트 초기설정

---
+++ src/main/java/challenge/nDaysChallenge/NDaysChallengeApplication.java
@@ -0,0 +1,13 @@
+package challenge.nDaysChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NDaysChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NDaysChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/nDaysChallenge/NDaysChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.nDaysChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NDaysChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
