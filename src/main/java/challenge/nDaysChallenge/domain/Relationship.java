5e82e513b7c437ad1c497daff10b1a5e0a80e8fb
yeeunchoii <104558374+yeeunchoii@users.noreply.github.com>
2022-11-08 12:59:58 +0900

    Ye (#3)
    * 내 브랜치 생성
    
    * 엔티티 수정 완료
    
    * 엔티티 수정2

--- src/main/java/challenge/nDaysChallenge/domain/Relationship.java
+++ src/main/java/challenge/nDaysChallenge/domain/Relationship.java
@@ -1,10 +1,8 @@
 package challenge.nDaysChallenge.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Entity
 public class Relationship {
@@ -12,10 +10,21 @@ public class Relationship {
     @Id
     @GeneratedValue
     @Column(name = "relationship_number")
-    private int number;
-    private int user_number;
-    private int friend_number;
-    private Date date;
-    private String status;
+    private Long number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_number")
+    private Member user_number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_number")
+    private Member friend_number;
+
+
+    private LocalDateTime date;
+
+    //친구신청 상태 enum으로 열거//
+    @Enumerated(EnumType.STRING)
+    private Enum status;
 
 }
