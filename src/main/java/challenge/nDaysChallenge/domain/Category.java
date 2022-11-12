7629fabdd932423ad62c23fa9bddbfd8778f91e8
5hk5 <104435274+5hk5@users.noreply.github.com>
2022-11-12 21:53:54 +0900

    Revert "Ye"

--- src/main/java/challenge/nDaysChallenge/domain/Category.java
+++
@@ -1,6 +0,0 @@
-package challenge.nDaysChallenge.domain;
-
-public enum Category {
-
-    MINDFULNESS, EXERCISE, ROUTINE, ETC
-}
--- src/main/java/challenge/nDaysChallenge/domain/RelationshipStatus.java
+++
@@ -1,9 +0,0 @@
-package challenge.nDaysChallenge.domain;
-
-public enum RelationshipStatus {
-
-     REQUEST , ACCEPT , REFUSE
-     //친구 신청, 수락, 거절//
-
-
-}
