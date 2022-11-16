0b367678b341b6ec3d9ebfbe2df29f8c5f7c107c
5hk5 <godqhr9798@gmail.com>
2022-11-16 20:25:27 +0900

    누락된 어노테이션 추가

--- src/main/java/challenge/nDaysChallenge/domain/Likes.java
+++ src/main/java/challenge/nDaysChallenge/domain/Likes.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-public class Like {
+public class Likes {
 
     @Id @GeneratedValue
     @Column(name = "like_number")
--- src/main/java/challenge/nDaysChallenge/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/challenge/nDaysChallenge/security/CustomAuthenticationEntryPoint.java
@@ -3,6 +3,7 @@ package challenge.nDaysChallenge.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import java.io.IOException;
 
 //인증X 사용자 접근 처리
 @Slf4j
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
