733b1dbb60fa8429011598822d8dfadded2137d7
5hk5 <godqhr9798@gmail.com>
2022-11-21 22:53:12 +0900

    다짐 컨트롤러 수정

--- src/main/java/challenge/nDaysChallenge/controller/dajim/DajimController.java
+++ src/main/java/challenge/nDaysChallenge/controller/dajim/DajimController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -40,7 +41,10 @@ public class DajimController {
                                                   @AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
         checkLogin(userDetailsImpl);
         List<Dajim> dajims = dajimService.viewDajimInRoom(roomNumber);
-        return ResponseEntity.ok().body(dajims);
+        List<DajimResponseDto> dajimsList = dajims.stream().map(dajim ->
+                new DajimResponseDto(dajim.getMember().getNickname(), dajim.getContent())
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dajimsList);
     }
 
     private void checkLogin(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
