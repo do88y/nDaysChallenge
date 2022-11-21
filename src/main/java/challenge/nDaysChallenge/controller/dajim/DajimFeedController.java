b40cd3b9e5f0fb657e77234bb739d5bb1fc96aa8
5hk5 <104435274+5hk5@users.noreply.github.com>
2022-11-21 22:17:49 +0900

    챌린지룸 내 다짐 도메인 완료, 피드 진행 중 (#37)
    * Dajim, DajimComment 엔티티 클래스  수정
    
    * Spring Security, Oauth, jwt 설정
    
    * 수정한 설정 파일 반영
    
    * gradle dependencies 추가
    
    * gradle dependencies 추가
    
    * 최신 파일 업데이트
    
    * Spring Security, jwt 로그인 관련 설정
    
    * 수정사항 반영
    
    * 누락된 어노테이션 추가
    
    * 예약어 오류 해결
    
    * 기타 오류 해결
    
    * likes 필드 삭제
    
    * 클래스명 및 컬럼명 변경
    
    * 다짐 패키지 생성 및 다짐 리포지토리 작성
    
    * 다짐 도메인 업데이트 (미완)
    
    * 시큐리티 컨텍스트 내 유저 관련 클래스 작성
    
    * 챌린지룸 내 다짐 도메인 완료, 피드 진행 중

---
+++ src/main/java/challenge/nDaysChallenge/controller/dajim/DajimFeedController.java
@@ -0,0 +1,60 @@
+package challenge.nDaysChallenge.controller.dajim;
+
+import challenge.nDaysChallenge.dto.response.DajimResponseDto;
+import challenge.nDaysChallenge.dto.response.LikesResponseDto;
+import challenge.nDaysChallenge.dto.response.dajimCommentsResponseDto;
+import challenge.nDaysChallenge.security.UserDetailsImpl;
+import challenge.nDaysChallenge.service.dajim.DajimFeedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class DajimFeedController { //피드 내 다짐
+
+    private final DajimFeedService dajimFeedService;
+
+    @GetMapping("/feed")
+    //다짐 피드 전체 (다짐 + 좋아요 + 댓글)
+    public ResponseEntity<?> viewDajimOnFeed(@PathVariable("roomNumber") Long roomNumber,
+                                                  @AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
+        //다짐, 좋아요, 댓글 List 대입
+        List<DajimResponseDto> dajimsList;
+        List<LikesResponseDto> likesList;
+        List<dajimCommentsResponseDto> dajimCommentsList;
+
+        try {
+            dajimsList = dajimFeedService.viewDajimOnFeed(roomNumber,userDetailsImpl);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+
+        //Map에 대입
+        Map<String, Object> dajimsListMap = new HashMap<>();
+        Map<String, Object> likesListMap = new HashMap<>();
+        Map<String, Object> dajimCommentsListMap = new HashMap<>();
+
+        dajimsListMap.put("다짐 리스트",dajimsList);
+//        likesListMap.put("좋아요 리스트",likesList);
+//        dajimCommentsListMap.put("댓글 리스트",dajimCommentsList);
+
+        //최종 List
+        List<Map> dajimFeed = new ArrayList<>();
+        dajimFeed.add(dajimsListMap);
+        dajimFeed.add(likesListMap);
+        dajimFeed.add(dajimCommentsListMap);
+
+        return ResponseEntity.ok().body(dajimFeed);
+    }
+
+}
---
+++ src/main/java/challenge/nDaysChallenge/repository/dajim/DajimFeedRepository.java
@@ -0,0 +1,22 @@
+package challenge.nDaysChallenge.repository.dajim;
+
+import challenge.nDaysChallenge.domain.dajim.Dajim;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Repository
+public interface DajimFeedRepository extends JpaRepository<Dajim, Long> { //피드 전체 다짐 조회
+    //전체 다짐 조회 - 같은 룸 멤버이거나 open이 PUBLIC 일 때
+    @Query("select d from Dajim d where d.roomNumber = :roomNumber or d.open = 'PUBLIC'")
+    List<Dajim> findAllByRoomNumberAndOpen(Long roomNumber);
+
+    //다짐별 좋아요
+
+
+    //다짐 내 댓글
+
+}
