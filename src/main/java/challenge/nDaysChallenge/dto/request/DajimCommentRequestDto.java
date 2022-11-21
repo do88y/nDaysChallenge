17c1ca32dfc78f51133810af252458e189acf7cf
5hk5 <godqhr9798@gmail.com>
2022-11-21 19:31:17 +0900

    챌린지룸 내 다짐 도메인 완료, 피드 진행 중

---
+++ src/main/java/challenge/nDaysChallenge/dto/request/DajimCommentRequestDto.java
@@ -0,0 +1,4 @@
+package challenge.nDaysChallenge.dto.request;
+
+public class DajimCommentRequestDto {
+}
---
+++ src/main/java/challenge/nDaysChallenge/dto/response/dajimCommentsResponseDto.java
@@ -0,0 +1,16 @@
+package challenge.nDaysChallenge.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class dajimCommentsResponseDto {
+
+
+
+}
