c0fd68191ebdc53a7becb1bfa46fd1fda509a463
5hk5 <104435274+5hk5@users.noreply.github.com>
2022-11-22 10:43:23 +0900

    다짐컨트롤러 수정
    다짐컨트롤러 수정

--- src/main/java/challenge/nDaysChallenge/controller/dajim/DajimController.java
+++ src/main/java/challenge/nDaysChallenge/controller/dajim/DajimController.java
@@ -1,19 +1,15 @@
 package challenge.nDaysChallenge.controller.dajim;
 
-<<<<<<< HEAD
+
 import challenge.nDaysChallenge.domain.Member;
 import challenge.nDaysChallenge.domain.dajim.Dajim;
 import challenge.nDaysChallenge.dto.request.DajimRequestDto;
 import challenge.nDaysChallenge.dto.response.DajimResponseDto;
-import challenge.nDaysChallenge.dto.response.MemberResponseDto;
 import challenge.nDaysChallenge.security.UserDetailsImpl;
 import challenge.nDaysChallenge.service.dajim.DajimService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,36 +17,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
-public class DajimController { ////dto->엔티티 전환 어떻게??어디서??
+public class DajimController {
 
     private final DajimService dajimService;
 
     //다짐 업로드, 수정
-    @GetMapping(name = "/challenge")
-    public ResponseEntity<?> uploadDajim(@RequestBody DajimRequestDto dajimRequestDto,
+    @GetMapping(name = "/challenge/{challengeId}/upload")
+    public ResponseEntity<?> uploadDajim(@PathVariable("challengeId") Long roomNumber,
+                                         @RequestBody DajimRequestDto dajimRequestDto,
                                          @AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
-        return ResponseEntity.ok(dajimService.uploadDajim(dajimRequestDto, userDetailsImpl));
+        checkLogin(userDetailsImpl);
+        Dajim dajim = dajimService.uploadDajim(roomNumber, dajimRequestDto, userDetailsImpl);
+        return ResponseEntity.ok().body(new DajimResponseDto(dajim.getMember().getNickname(),dajim.getContent()));
     }
 
-
-    //챌린지 상세화면 - 각 Room 다짐 조회
-    @GetMapping(name = "/challenge/{roomMemberNumber}")
-    public ResponseEntity<?> viewDajimOnChallenge(@PathVariable("roomMemberNumber") Long roomNumber,
+    //다짐 조회
+    @GetMapping(name = "/challenge/{challengeId}")
+    public ResponseEntity<?> viewDajimOnChallenge(@PathVariable("challengeId") Long roomNumber,
                                                   @AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
-        return ResponseEntity.ok(dajimService.viewDajimOnChallenge(roomNumber, userDetailsImpl));
+        checkLogin(userDetailsImpl);
+        List<Dajim> dajims = dajimService.viewDajimInRoom(roomNumber);
+        List<DajimResponseDto> dajimsList = dajims.stream().map(dajim ->
+                new DajimResponseDto(dajim.getMember().getNickname(), dajim.getContent())
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dajimsList);
     }
 
-    //피드 - 전체 다짐 리스트 조회
-    @GetMapping(name = "/feed")
-    public ResponseEntity<?> viewDajimOnFeed(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
-        return ResponseEntity.ok(dajimService.viewDajimOnFeed(userDetailsImpl));
+    private void checkLogin(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+        if (userDetailsImpl == null) {
+            throw new RuntimeException("로그인한 멤버만 사용할 수 있습니다.");
+        }
     }
 
 }
-=======
-public class DajimController {
-}
->>>>>>> develop
