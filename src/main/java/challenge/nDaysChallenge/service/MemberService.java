c24d11c0a27eb5c79af74a56be7dbc8d20dfda15
yeeunchoii <dpdmd0106@gmail.com>
2022-11-24 13:44:36 +0900

    20221124 relationship 엔티티,서비스,리포지토리

--- src/main/java/challenge/nDaysChallenge/service/MemberService.java
+++ src/main/java/challenge/nDaysChallenge/service/MemberService.java
@@ -1,11 +1,8 @@
 package challenge.nDaysChallenge.service;
 import challenge.nDaysChallenge.domain.Member;
 import challenge.nDaysChallenge.domain.MemberSignUpDto;
-<<<<<<< HEAD
 import challenge.nDaysChallenge.repository.MemberRepository;
 import challenge.nDaysChallenge.dto.response.MemberResponseDto;
-=======
->>>>>>> refs/remotes/origin/develop
 import challenge.nDaysChallenge.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,29 +18,21 @@ public class MemberService implements MemberServiceIn {
     private final PasswordEncoder passwordEncoder;
 
 
-<<<<<<< HEAD
-    @Transactional
-    @Override
-    public String signUp(MemberSignUpDto signUpDto) throws Exception {
-
-=======
-
     @Transactional
     @Override
     public String signUp(MemberSignUpDto signUpDto) throws Exception {
 
 
->>>>>>> refs/remotes/origin/develop
-        if (memberRepository.existsById(signUpDto.getId()) == true) {
+        if (memberRepository.existsById(signUpDto.getId())) {
             throw new Exception("이미 존재하는 이메일입니다.");
         }
 
-<<<<<<< HEAD
+
         if(!signUpDto.getPw().equals(signUpDto.getPw())){
             throw new Exception("비밀번호가 일치하지않습니다.");
         }
 
-        if (memberRepository.existsByNickname(signUpDto.getNickname()) == true) {
+        if (memberRepository.existsByNickname(signUpDto.getNickname())) {
             throw new Exception("이미 존재하는 닉네임입니다.");
         }
 
@@ -51,26 +40,10 @@ public class MemberService implements MemberServiceIn {
         member.encodePassword(passwordEncoder);
         member.authority();
         return signUpDto.getId();
-=======
-        if (memberRepository.existsByNickname(signUpDto.getNickname()) == true) {
-            throw new Exception("이미 존재하는 닉네임입니다.");
-        }
 
-        Member member = memberRepository.save(signUpDto.memberToEntity());
-        member.encodePassword(passwordEncoder);
-        member.authority();
 
-        return signUpDto.getId();
-
->>>>>>> refs/remotes/origin/develop
     }
 }
 
 
 
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> refs/remotes/origin/develop
