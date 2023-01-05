1d003afc3dc471fea2adbdcd379a78de675ca75a
do88y <appassionato.seo@gmail.com>
2023-01-06 07:26:11 +0900

    신고 컨트롤러에 로그인 검증 코드 추가

--- src/main/java/challenge/nDaysChallenge/controller/ReportController.java
+++ src/main/java/challenge/nDaysChallenge/controller/ReportController.java
@@ -1,12 +1,15 @@
 package challenge.nDaysChallenge.controller;
 
 import challenge.nDaysChallenge.domain.Report;
+import challenge.nDaysChallenge.domain.member.Member;
+import challenge.nDaysChallenge.domain.member.MemberAdapter;
 import challenge.nDaysChallenge.dto.request.ReportRequestDto;
 import challenge.nDaysChallenge.dto.response.ReportResponseDto;
 import challenge.nDaysChallenge.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +22,10 @@ public class ReportController {
 
     //신고 생성
     @PostMapping("/feed/report")
-    public ResponseEntity<?> report(@RequestBody ReportRequestDto dto) {
+    public ResponseEntity<?> report(@AuthenticationPrincipal MemberAdapter memberAdapter,
+                                    @RequestBody ReportRequestDto dto) {
+
+        checkLogin(memberAdapter.getMember());
 
         Report report = reportService.report(dto.getDajim(), dto.getCause(), dto.getIsDajim(), dto.getContent());
         ReportResponseDto savedReport = ReportResponseDto.builder()
@@ -31,5 +37,12 @@ public class ReportController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedReport);
+    }
+
+    //로그인 검증
+    private void checkLogin(Member member) {
+        if (member == null) {
+            throw new RuntimeException("로그인한 멤버만 사용할 수 있습니다.");
+        }
     }
 }
