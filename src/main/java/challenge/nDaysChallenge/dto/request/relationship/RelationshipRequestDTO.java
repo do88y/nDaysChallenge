266f57fd70a0bcc39a61771d5571f986b6b09a22
yeeunchoii <dpdmd0106@gmail.com>
2023-01-13 14:32:54 +0900

    0113친구요청리스트 코드수정/테스트완료

--- src/main/java/challenge/nDaysChallenge/dto/request/relationship/RelationshipRequestDTO.java
+++ src/main/java/challenge/nDaysChallenge/dto/request/relationship/RelationshipRequestDTO.java
@@ -12,12 +12,18 @@ public class RelationshipRequestDTO {
 
   private String  id;                   //친구아이디
 
+  private String nickname;       //친구 닉네임
+
+  private int image;                  //친구 프로필사진
+
   @DateTimeFormat(pattern = "yyyy-mm-dd HH:mm:ss")
   private LocalDateTime requestedDate;    //요청날짜시간
 
 
 
-  public RelationshipRequestDTO(String id){
+  public RelationshipRequestDTO(String id, String nickname, int image){
     this.id=id;
+    this.nickname=nickname;
+    this.image=image;
   }
 }
